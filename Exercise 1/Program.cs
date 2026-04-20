1ea875c253d5b7b28fc5fcfb3129c50f1dd5688a
rinaetshivhase <rinaetshivhase02@gmail.com>
2026-04-20 19:27:01 +0200

    adding changes

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // You have two arrays/lists as follows
+            int[] list1 = new int[] { 1, 2, 3, 4, 5 };
+            int[] list2 = new int[] { 3, 4, 5, 6, 7 };
+
+            // a. Common elements in both lists
+            var list3 = list1.Intersect(list2).ToArray();
+            Console.WriteLine(string.Join(" ", list3));
+
+            // b. Elements in list1 that are not found in list2
+            var list4 = list1.Except(list2).ToArray();
+            Console.WriteLine(string.Join(" ", list4));
+
+            // c. Elements in list2 that are not found in list1
+            var list5 = list2.Except(list1).ToArray();
+            Console.WriteLine(string.Join(" ", list5));
+
+            Console.WriteLine("Press <ENTER> to continue");
+            Console.ReadLine();
+        }
+    }
+}
